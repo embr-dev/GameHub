ce6e75d2b62d5149ac3839926b1c2d43cbf91e66
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-02 09:00:35 -0700

    added database

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,19 +1,15 @@
-setTimeout(() => {
-    const firebaseConfig = {
-        apiKey: "AIzaSyCljTB8jYkhyf_XRlbcRk6ai2c1kmTwSpQ",
-        authDomain: "gamehub-527d9.firebaseapp.com",
-        projectId: "gamehub-527d9",
-        storageBucket: "gamehub-527d9.appspot.com",
-        messagingSenderId: "609224844553",
-        appId: "1:609224844553:web:4e5e898220b26b594ddfd8"
-    };
+const firebaseConfig = {
+    apiKey: "AIzaSyCljTB8jYkhyf_XRlbcRk6ai2c1kmTwSpQ",
+    authDomain: "gamehub-527d9.firebaseapp.com",
+    projectId: "gamehub-527d9",
+    storageBucket: "gamehub-527d9.appspot.com",
+    messagingSenderId: "609224844553",
+    appId: "1:609224844553:web:4e5e898220b26b594ddfd8"
+};
 
+setTimeout(() => {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
-
-    // Initialize Realtime Database and get a reference to the service
-    const database = firebase.database();
 
     const form = document.getElementById('signup');
     const username = document.getElementById('username');
