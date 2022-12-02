98cb21f4ab1bf30bc86ecf335a2710aff65f07c0
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-02 13:04:52 -0700

    fix

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -7,174 +7,172 @@ const firebaseConfig = {
     appId: "1:609224844553:web:4e5e898220b26b594ddfd8"
 };
 
-setTimeout(() => {
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    const form = document.getElementById('signup');
-    const username = document.getElementById('username');
-    const pswrd = document.getElementById('pswrd');
-    const db = firebase.database().ref('accounts');
-    var isTaken;
-    var isCreated = false;
-    var isLoaded = 0;
-    var avatar;
-    isLoaded++
-    function newAvatar() {
-        const word = username.value;
-        let peer;
-        peer = new Peer();
-        peer.on("open", (id) => {
-            var ifr = document.createElement("iframe");
-            ifr.src = `https://russell2259.github.io/barbackend/Avatar?token=${id}&word=${word}`;
-            ifr.style = "width: 0px; height: 0px;";
-            ifr.frameBorder = 'none'
-            ifr.id = 'ifr'
-            document.body.appendChild(ifr);
+const form = document.getElementById('signup');
+const username = document.getElementById('username');
+const pswrd = document.getElementById('pswrd');
+const accountsDB = firebase.database().ref('accounts');
+var isTaken;
+var isCreated = false;
+var isLoaded = 0;
+var avatar;
+isLoaded++
+function newAvatar() {
+    const word = username.value;
+    let peer;
+    peer = new Peer();
+    peer.on("open", (id) => {
+        var ifr = document.createElement("iframe");
+        ifr.src = `https://russell2259.github.io/barbackend/Avatar?token=${id}&word=${word}`;
+        ifr.style = "width: 0px; height: 0px;";
+        ifr.frameBorder = 'none'
+        ifr.id = 'ifr'
+        document.body.appendChild(ifr);
+    });
+    peer.on('connection', (conn) => connection(conn, 'avatar'));
+}
+function connection(conn, parent) {
+    const decrypt = (salt, encoded) => {
+        const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+        const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+        return encoded
+            .match(/.{1,2}/g)
+            .map((hex) => parseInt(hex, 16))
+            .map(applySaltToChar)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join("");
+    };
+    conn.on("open", () => {
+        conn.on('data', (data) => {
+            if (parent === 'avatar') {
+                const message = decrypt('avatar', data)
+                avatar = message;
+                isLoaded++
+            }
         });
-        peer.on('connection', (conn) => connection(conn, 'avatar'));
+    });
+}
+function displayErr(err, elid) {
+    document.querySelector('.form').classList.remove('hidden')
+    document.querySelector('.Loader').classList.add('hidden')
+    document.getElementById(elid).innerText = err
+}
+function clearErrs() {
+    const err = document.querySelectorAll('.err');
+    for (let i = 0; i < err.length; i++) {
+        err[i].innerText = '';
     }
-    function connection(conn, parent) {
-        const decrypt = (salt, encoded) => {
-            const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-            const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
-            return encoded
-                .match(/.{1,2}/g)
-                .map((hex) => parseInt(hex, 16))
-                .map(applySaltToChar)
-                .map((charCode) => String.fromCharCode(charCode))
-                .join("");
-        };
-        conn.on("open", () => {
-            conn.on('data', (data) => {
-                if (parent === 'avatar') {
-                    const message = decrypt('avatar', data)
-                    avatar = message;
-                    isLoaded++
-                }
-            });
-        });
-    }
-    function displayErr(err, elid) {
-        document.querySelector('.form').classList.remove('hidden')
-        document.querySelector('.Loader').classList.add('hidden')
-        document.getElementById(elid).innerText = err
-    }
-    function clearErrs() {
-        const err = document.querySelectorAll('.err');
-        for (let i = 0; i < err.length; i++) {
-            err[i].innerText = '';
-        }
-    }
-    window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
-        console.log('An internal error occurred. Please try again later.');//or any message
-        return false;
-    }
-    function f2(users, user) {
-        db.on('value', function (accounts) {
-            const data = accounts.val();
+}
+window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+    console.log('An internal error occurred. Please try again later.');//or any message
+    return false;
+}
+function f2(users, user) {
+    accountsDB.on('value', function (accounts) {
+        const data = accounts.val();
 
-            var interval = setInterval(() => {
-                if (isLoaded === 2) {
-                    clearInterval(interval)
-                    if (data) {
-                        user =
-                        {
-                            username: username.value,
-                            password: pswrd.value,
-                            pridevid: localStorage.getItem('devid'),
-                            history: [],
-                            friends: [],
-                            settings: 'default',
-                            pfp: avatar,
-                            id: data.length + 1
-                        };
-                        localStorage.setItem('isLogin', true);
-                        localStorage.setItem('userId', data.length + 1);
-                        isCreated = true;
-                        if (!data) {
-                            users.push(user);
-                            db.set(users);
-                        } else {
-                            users = data;
-                            users.push(user);
-                            db.set(users);
-                        }
-                        isCreated = true;
+        var interval = setInterval(() => {
+            if (isLoaded === 2) {
+                clearInterval(interval)
+                if (data) {
+                    user =
+                    {
+                        username: username.value,
+                        password: pswrd.value,
+                        pridevid: localStorage.getItem('devid'),
+                        history: [],
+                        friends: [],
+                        settings: 'default',
+                        pfp: avatar,
+                        id: data.length + 1
+                    };
+                    localStorage.setItem('isLogin', true);
+                    localStorage.setItem('userId', data.length + 1);
+                    isCreated = true;
+                    if (!data) {
+                        users.push(user);
+                        accountsDB.set(users);
                     } else {
-                        user =
-                        {
-                            username: username.value,
-                            password: pswrd.value,
-                            pridevid: localStorage.getItem('devid'),
-                            history: [],
-                            friends: [],
-                            settings: 'default',
-                            pfp: avatar,
-                            id: 1
-                        };
-                        localStorage.setItem('isLogin', true);
-                        localStorage.setItem('userId', 1);
-                        isCreated = true;
-                        if (!data) {
-                            users.push(user);
-                            db.set(users);
-                        } else {
-                            users = data;
-                            users.push(user);
-                            db.set(users);
-                        }
-                        isCreated = true;
+                        users = data;
+                        users.push(user);
+                        accountsDB.set(users);
                     }
-                }
-            }, 20);
-        })
-    }
-
-    function f1() {
-        db.on('value', function (accounts) {
-            const data = accounts.val();
-            if (data) {
-                let usernames = [];
-                for (let i = 0; i < data.length; i++) {
-                    usernames.push(data[i].username)
-                }
-                if (usernames.includes(username.value) === false) {
-                    newAvatar()
-                    f2([], {})
+                    isCreated = true;
                 } else {
-                    displayErr('This username has already been taken', 'usernameErr');
+                    user =
+                    {
+                        username: username.value,
+                        password: pswrd.value,
+                        pridevid: localStorage.getItem('devid'),
+                        history: [],
+                        friends: [],
+                        settings: 'default',
+                        pfp: avatar,
+                        id: 1
+                    };
+                    localStorage.setItem('isLogin', true);
+                    localStorage.setItem('userId', 1);
+                    isCreated = true;
+                    if (!data) {
+                        users.push(user);
+                        accountsDB.set(users);
+                    } else {
+                        users = data;
+                        users.push(user);
+                        accountsDB.set(users);
+                    }
+                    isCreated = true;
                 }
-            } else {
+            }
+        }, 20);
+    })
+}
+
+function f1() {
+    accountsDB.on('value', function (accounts) {
+        const data = accounts.val();
+        if (data) {
+            let usernames = [];
+            for (let i = 0; i < data.length; i++) {
+                usernames.push(data[i].username)
+            }
+            if (usernames.includes(username.value) === false) {
                 newAvatar()
                 f2([], {})
-            }
-        });
-    }
-    username.focus();
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        clearErrs();
-        if (!username.value || !pswrd.value) {
-            if (!pswrd.value) {
-                displayErr('Please fill out this field', 'pswrdErr');
-                pswrd.focus();
-            }
-            if (!username.value) {
-                displayErr('Please fill out this field', 'usernameErr');
-                username.focus();
+            } else {
+                displayErr('This username has already been taken', 'usernameErr');
             }
         } else {
-            f1();
-            document.querySelector('.Loader').classList.remove('hidden')
-            document.querySelector('.form').classList.add('hidden')
-            var interval = setInterval(() => {
-                if (isCreated === true) {
-                    clearInterval(interval)
-                    document.querySelector('#loadingText').innerText = 'Logging you in...'
-                    window.location.href = `/home?ref=${window.location.href}&did=${localStorage.getItem('devid')}&uid=${localStorage.getItem('userId')}&uft=true`
-                }
-            }, 500);
+            newAvatar()
+            f2([], {})
         }
     });
-}, 1000);
+}
+username.focus();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    clearErrs();
+    if (!username.value || !pswrd.value) {
+        if (!pswrd.value) {
+            displayErr('Please fill out this field', 'pswrdErr');
+            pswrd.focus();
+        }
+        if (!username.value) {
+            displayErr('Please fill out this field', 'usernameErr');
+            username.focus();
+        }
+    } else {
+        f1();
+        document.querySelector('.Loader').classList.remove('hidden')
+        document.querySelector('.form').classList.add('hidden')
+        var interval = setInterval(() => {
+            if (isCreated === true) {
+                clearInterval(interval)
+                document.querySelector('#loadingText').innerText = 'Logging you in...'
+                window.location.href = `/home?ref=${window.location.href}&did=${localStorage.getItem('devid')}&uid=${localStorage.getItem('userId')}&uft=true`
+            }
+        }, 500);
+    }
+});
