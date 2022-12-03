f5dee7edabb8d22999d0294a7a49e4b20b9556f3
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-02 17:05:10 -0700

    =(

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,14 +1,15 @@
 const firebaseConfig = {
     apiKey: "AIzaSyCljTB8jYkhyf_XRlbcRk6ai2c1kmTwSpQ",
     authDomain: "gamehub-527d9.firebaseapp.com",
+    databaseURL: "https://gamehub-527d9-default-rtdb.firebaseio.com",
     projectId: "gamehub-527d9",
     storageBucket: "gamehub-527d9.appspot.com",
     messagingSenderId: "609224844553",
-    appId: "1:609224844553:web:4e5e898220b26b594ddfd8"
-};
+    appId: "1:609224844553:web:dc43596c5145a7d64ddfd8"
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const database = firebase.database();
 
 database.ref(accounts).on('value', (snapshot) => {
