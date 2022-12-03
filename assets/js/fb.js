b403220b3775117740a44fe36d04a16b511e2ff0
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-02 17:59:19 -0700

    update

--- assets/js/fb.js
+++ assets/js/fb.js
@@ -1,11 +1,19 @@
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCljTB8jYkhyf_XRlbcRk6ai2c1kmTwSpQ",
   authDomain: "gamehub-527d9.firebaseapp.com",
+  databaseURL: "https://gamehub-527d9-default-rtdb.firebaseio.com",
   projectId: "gamehub-527d9",
   storageBucket: "gamehub-527d9.appspot.com",
   messagingSenderId: "609224844553",
-  appId: "1:609224844553:web:4e5e898220b26b594ddfd8"
+  appId: "1:609224844553:web:dc43596c5145a7d64ddfd8"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);*/
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+const accountsDB = database.ref('accounts');
+
+accountsDB.on("value", function (data) {
+  console.log(data.val());
+})
