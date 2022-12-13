2869c15c6374f9af7dbf43a534a215b5a8e901f6
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-13 13:27:48 -0700

    added error stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@ window.history.pushState({}, '', window.location.pathname);
 const urlParams = new URLSearchParams(queryString);
 const action = urlParams.get('action');
 
+window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+    console.log('Error: ' + errorMsg + '\n\nUrl: ' + url + '\n\nLine:' + lineNumber);
+    return false;
+}
+
 function error(errCode) {
     fetch('/assets/JSON/errors.json')
         .then(resp => resp.json())
