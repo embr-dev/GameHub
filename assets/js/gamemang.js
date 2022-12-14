bbd4614fcecb55a25ccac01287d4fa1c79884c69
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-14 12:31:51 -0700

    removed firebug

--- assets/js/gamemang.js
+++ assets/js/gamemang.js
@@ -1,5 +1,3 @@
-javascript:var firebug=document.createElement('script');firebug.setAttribute('src','https://luphoria.com/fbl/fbl/firebug-lite-debug.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);
-
 const nav = document.querySelector('.navbar');
 fetch('https://api.retronetwork.ml/GameHub/games')
     .then(obj => obj.json())
