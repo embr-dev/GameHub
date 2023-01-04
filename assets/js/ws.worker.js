11bbd092f472a73151995a608248d5cb4765d640
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-04 14:46:39 -0700

    added socket reconnection

--- assets/js/ws.worker.js
+++ assets/js/ws.worker.js
@@ -1,46 +1,59 @@
+let socket = new WebSocket('wss://api.retronetwork.ml');
+
 onmessage = (e) => {
-    const socket = new WebSocket('wss://api.retronetwork.ml');
-    const sessionId = e.data.ssid;
-    const userId = e.data.suid;
+    if (socket.readyState === 1) {
+        const sessionId = e.data.ssid;
+        const userId = e.data.suid;
 
-    socket.addEventListener('open', (event) => {
-        socket.send(JSON.stringify({
-            sessionId: sessionId,
-            userId: userId
-        }))
+        socket.addEventListener('open', (event) => {
+            socket.send(JSON.stringify({
+                sessionId: sessionId,
+                userId: userId
+            }))
+        });
 
-    });
+        socket.addEventListener('message', (event) => {
+            let msg;
 
-    socket.addEventListener('message', (event) => {
-        let msg;
-
-        try {
-            msg = JSON.parse(event.data);
-        } catch (err) {
-            console.log('Server sent invalid data type');
-        }
-
-        if (msg) {
-            if (msg.error === false) {
-                if (msg.targets.includes(sessionId)) {
-                    postMessage({
-                        type: 'self',
-                        data: msg.data
-                    });
-                } else if (msg.targets.includes('all')) {
-                    postMessage({
-                        type: 'brodcast',
-                        data: msg.data
-                    });
-                }
-            } else if (msg.error === true) {
-                postMessage({
-                    error: true,
-                    errorMsg: msg.errorMsg
-                })
-
-                console.log(msg.errorMsg);
+            try {
+                msg = JSON.parse(event.data);
+            } catch (err) {
+                console.log('Server sent invalid data type');
             }
-        }
-    });
+
+            if (msg) {
+                if (msg.error === false) {
+                    if (msg.targets.includes(sessionId)) {
+                        postMessage({
+                            type: 'self',
+                            data: msg.data
+                        });
+                    } else if (msg.targets.includes('all')) {
+                        postMessage({
+                            type: 'brodcast',
+                            data: msg.data
+                        });
+                    }
+                } else if (msg.error === true) {
+                    postMessage({
+                        error: true,
+                        errorMsg: msg.errorMsg
+                    })
+
+                    console.log(msg.errorMsg);
+                }
+            }
+        });
+    } else {
+        postMessage({
+            error: true,
+            errorMsg: 'Websocket not avalible'
+        })
+    }
 }
+
+setInterval(() => {
+    if (socket.readyState === 3) {
+        socket = new WebSocket('wss://api.retronetwork.ml');
+    }
+}, 1000);
