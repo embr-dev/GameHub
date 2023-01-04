5a2130a6bd73a51aa83184ce5f7c0378018593f7
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-04 12:37:22 -0700

    tried to fix ws worker

--- assets/js/ws.worker.js
+++ assets/js/ws.worker.js
@@ -1,49 +1,42 @@
 onmessage = (e) => {
     const socket = new WebSocket('wss://api.retronetwork.ml');
 
-    if (socket) {
-        socket.addEventListener('open', (event) => {
-            socket.send(JSON.stringify({
-                sessionId: sessionStorage.getItem('session')
-            }))
-        });
+    socket.addEventListener('open', (event) => {
+        socket.send(JSON.stringify({
+            sessionId: sessionStorage.getItem('session')
+        }))
+    });
 
-        socket.addEventListener('message', (event) => {
-            let msg;
-            
-            try {
-                msg = JSON.parse(event.data);
-            } catch (err) {
-                console.log('Server sent invalid data type');
-            }
+    socket.addEventListener('message', (event) => {
+        let msg;
 
-            if (msg) {
-                if (msg.error === false) {
-                    if (msg.targets.includes(sessionStorage.getItem('session'))) {
-                        postMessage({
-                            type: 'self',
-                            data: msg.data
-                        });
-                    } else if (msg.targets.includes('all')) {
-                        postMessage({
-                            type: 'brodcast',
-                            data: msg.data
-                        });
-                    }
-                } else if (msg.error === true) {
+        try {
+            msg = JSON.parse(event.data);
+        } catch (err) {
+            console.log('Server sent invalid data type');
+        }
+
+        if (msg) {
+            if (msg.error === false) {
+                if (msg.targets.includes(sessionStorage.getItem('session'))) {
                     postMessage({
-                        error: true,
-                        errorMsg: msg.errorMsg
-                    })
-
-                    console.log(msg.errorMsg);
+                        type: 'self',
+                        data: msg.data
+                    });
+                } else if (msg.targets.includes('all')) {
+                    postMessage({
+                        type: 'brodcast',
+                        data: msg.data
+                    });
                 }
+            } else if (msg.error === true) {
+                postMessage({
+                    error: true,
+                    errorMsg: msg.errorMsg
+                })
+
+                console.log(msg.errorMsg);
             }
-        });
-    } else {
-        postMessage({
-            error: true,
-            errorMsg: 'Invalid server url'
-        })
-    }
+        }
+    });
 }
