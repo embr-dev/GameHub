61cba13c690df136d2770b7e350f7186bf64acaa
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2024-01-08 07:52:56 -0700

    update analytics

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -3,18 +3,29 @@ import API from './api.js';
 export default () => {
     if (localStorage.getItem('analytics_website_id') !== 'not tracked') {
         if (localStorage.getItem('analytics_website_id')) {
-            const analytics = document.createElement('script');
-            analytics.setAttribute('data-website-id', localStorage.getItem('analytics_website_id'));
-            analytics.setAttribute('async', '');
-            analytics.src = 'https://analytics.embernet.work/script.js';
-            document.head.insertBefore(analytics, document.head.firstChild);
+            const analyticsScript = document.createElement('script');
+            analyticsScript.setAttribute('data-website-id', localStorage.getItem('analytics_website_id'));
+            analyticsScript.setAttribute('async', '');
+            analyticsScript.src = 'https://analytics.embernet.work/script.js';
+            document.head.insertBefore(analyticsScript, document.head.firstChild);
+
+            analyticsScript.onload = () => window.analytics = window.umami;
         } else API.get(`/analytics/${window.location.hostname}`)
             .then(res => {
                 if (typeof res === 'string') {
                     localStorage.setItem('analytics_website_id', res);
                     location.reload();
-                } else localStorage.setItem('analytics_website_id', 'not tracked');
+                } else {
+                    localStorage.setItem('analytics_website_id', 'not tracked');
+                    location.reload();
+                }
             })
-            .catch(e => localStorage.setItem('analytics_website_id', 'not tracked'));
-    }
+            .catch(e => {
+                localStorage.setItem('analytics_website_id', 'not tracked');
+                location.reload();
+            });
+    } else window.analytics = {
+        track: (...args) => {},
+        identify: (...args) => {}
+    };
 };
